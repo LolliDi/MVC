453bc03356777c169864d5fb439d68c7c842031f
Lolli Di <290romchik@gmail.com>
2022-01-26 15:59:02 +0300

    Добавьте файлы проекта.

---
+++ MVC/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.TBoxNum1 = TBoxNum1;
+            Model.TBoxNum2 = TBoxNum2;
+            Model.TBlockOperation = TBlockOperation;
+            Model.TBlockAnswer = TBlockAnswer;
+            CBOperation.ItemsSource = Model.listOperations;
+            CBOperation.SelectedIndex = 0;
+        }
+
+        private void CBOperation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.SelectCombo = CBOperation.SelectedIndex;
+        }
+
+        private void BtnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            Model.Counting();
+        }
+    }
+}
---
+++ MVC/Model.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    public static class Model
+    {
+        public static TextBlock TBlockAnswer;
+        public static TextBlock TBlockOperation;
+        public static TextBox TBoxNum1;
+        public static TextBox TBoxNum2;
+
+        public static List<string> listOperations = new List<string>() {"Сложение","Вычитание","Умножение","Деление"};
+        static int selectedOperation;
+
+        public static int SelectCombo
+        {
+            set
+            {
+                selectedOperation = value;
+                switch(value)
+                {
+                    case 0: TBlockOperation.Text = "+";
+                        break;
+                    case 1: TBlockOperation.Text = "-";
+                        break;
+                    case 2:
+                        TBlockOperation.Text = "х";
+                        break;
+                    case 3:
+                        TBlockOperation.Text = "/";
+                        break;
+                }
+            }
+        }
+
+        public static void Counting()
+        {
+            try
+            {
+                double firstNum = Convert.ToDouble(TBoxNum1.Text);
+                double secondNum = Convert.ToDouble(TBoxNum2.Text);
+                if (Double.IsNaN(firstNum) || Double.IsNaN(secondNum))
+                {
+                    throw new Exception("Введите цифры для выполнения операций!");
+                }
+                switch (selectedOperation)
+                {
+                    case 0:
+                        TBlockAnswer.Text = ""+(firstNum + secondNum);
+                        break;
+                    case 1:
+                        TBlockAnswer.Text = "" + (firstNum - secondNum);
+                        break;
+                    case 2:
+                        TBlockAnswer.Text = "" + (firstNum * secondNum);
+                        break;
+                    case 3:
+                        TBlockAnswer.Text = "" + (firstNum / secondNum);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+           
+        }
+    }
+}
